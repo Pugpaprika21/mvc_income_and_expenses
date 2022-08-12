9d6ed81756dd422d92d7ae79dfccd835d621f2f1
Pugpaprika21 <pugpaprika21@gmail.com>
2022-08-12 05:11:09 -0700

    e

--- index.php
+++ index.php
@@ -6,61 +6,39 @@ use Pug_Framework\Include\Autoload\Autoloader;
 require_once dirname(__DIR__) . '../mvc_income_and_expenses/pug_framework/include/autoload/Autoload.php';
 
 define('load', Autoloader::register());
+/**
+ * @return object
+ */
+function displayURL(): object
+{
+    $httpStr = (new HttpString())
+        ->getUrlComponents('https://www.geeksforgeeks.org?name=Tonny&age=24')
+        ->showOne('query');
+    return $httpStr;
+}
 
-$httpStr = (new HttpString())
-    ->getUrlComponents('https://www.geeksforgeeks.org?name=Tonny&age=24')
-    ->showOne('query');
 
-print_r($httpStr);
+class URLcurrent
+{
+    public $getUrl = '';
 
-?>
+    public function currentHTTP(): object
+    {
+        $url = '';
+        $protocol = '';
 
-<!doctype html>
-<html lang="en">
+        if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) {
+            $protocol .= 'https://';
+        } else {
+            $protocol .= 'http://';
+        }
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-</head>
+        $url .= $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        $this->getUrl = $url;
 
-<body>
-    <!--  -->
+        return $this;
+    }
+}
 
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <br>
-
-    <div class="container">
-
-    </div>
-
-    <!--  -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-</body>
-
-</html>
+$urlNow = (new URLcurrent())->currentHTTP();
+print_r($urlNow);
