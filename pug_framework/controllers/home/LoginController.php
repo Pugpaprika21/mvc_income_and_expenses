de7d84817dd4df0d020c15d45d73b874674c28a7
Pugpaprika21 <pugpaprika21@gmail.com>
2022-08-13 23:10:55 -0700

    e

--- pug_framework/controllers/home/LoginController.php
+++ pug_framework/controllers/home/LoginController.php
@@ -2,6 +2,9 @@
 
 namespace Pug_Framework\Controllers\Home;
 
+use Pug_Framework\Http\Http_Response\Response;
+use Pug_Framework\Model\Query_Builder\Query;
+
 require_once dirname(__DIR__) . '../../../pug_framework/include/autoload/Autoload.php';
 
 class LoginController
@@ -13,6 +16,13 @@ class LoginController
      */
     public function login(object $request): void
     {
-        print_r($request);
+        $sqlCheck = "SELECT username, password, role FROM user_tb WHERE username =:username AND password =:password";
+
+        $resQuery = (new Query())->select($sqlCheck, [
+            'username' => $request->username,
+            'password' => $request->password
+        ]);
+
+        Response::render($resQuery)->jsonString();
     }
 }
--- pug_framework/controllers/home/RegisterController.php
+++ pug_framework/controllers/home/RegisterController.php
@@ -2,9 +2,46 @@
 
 namespace Pug_Framework\Controllers\Home;
 
+use Pug_Framework\Helper_Function\Tool\CreateUrl;
+use Pug_Framework\Model\Query_Builder\Query;
+
 require_once dirname(__DIR__) . '../../../pug_framework/include/autoload/autoload.php';
 
 class RegisterController
 {
+    /**
+     * @param object $request
+     * @return void
+     */
+    public function register(object $request): void
+    {
+        $sqlSelUniq = "SELECT username, password FROM user_tb WHERE username =:username AND password =:password";
+        
+        $selectCheck = (new Query())->select($sqlSelUniq, [
+            'username' => $request->username,
+            'password' => $request->password
+        ]);
 
+        $sqlRegister = "INSERT INTO user_tb(username, password, gmail, phone, image, datetime, role) VALUES(:username, :password, :gmail, :phone, :image, :datetime, :role)";
+      
+        $checkQuery = (new Query())->insert($sqlRegister, [
+            'username' => $request->username,
+            'password' => $request->password,
+            'gmail' => $request->gmail,
+            'phone' => $request->phone,
+            'image' => $request->image,
+            'datetime' => $request->datetime,
+            'role' => $request->role,
+        ]);
+
+        if ($checkQuery) {
+            $locationViewLogin = '../../../../mvc_income_and_expenses/pug_framework/resource/view/home/login.php';
+            CreateUrl::display_path($locationViewLogin)->withQueryString([
+                'status' => 200,
+                'message' => 'insert successfully'
+            ]);
+        }
+
+        return;
+    }
 }
